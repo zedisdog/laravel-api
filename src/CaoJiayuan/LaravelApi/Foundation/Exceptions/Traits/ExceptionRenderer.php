c3db945201a676d61851f9a3fcc1f6d0ef3aef42
Cao Jiayuan <cjy632258@hotmail.com>
2017-11-01 18:06:38 +0800

    exception handler

--- src/CaoJiayuan/LaravelApi/Foundation/Exceptions/Traits/ExceptionRenderer.php
+++ src/CaoJiayuan/LaravelApi/Foundation/Exceptions/Traits/ExceptionRenderer.php
@@ -38,11 +38,67 @@ trait ExceptionRenderer
                 $errors = $exception->validator->getMessageBag();
                 $message = $errors->first();
             }
-            return response()->json(['code' => $code, 'errors' => $errors, 'message' => $message], $code);
+            $data = ['code' => $code, 'errors' => $errors, 'message' => $message];
+            if (config('app.debug') && $code == 500) {
+                $file = $exception->getFile();
+                $file = str_replace(base_path() . DIRECTORY_SEPARATOR, '', $file);
+                $line = $exception->getLine();
+                $data['file'] =  "$file. Line:[$line]";
+                $data['trace'] = $this->parseTrace($exception);
+            }
+
+            return response()->json($data, $code);
         }
         if ($code == 200) {
             return response($message);
         }
         return parent::render($request, $exception);
+    }
+
+    /**
+     * @param Exception $exception
+     * @return array
+     */
+    public function parseTrace($exception)
+    {
+        $t = $exception->getTrace();
+
+        $trace = [];
+
+        foreach ($t as $i => $item) {
+            $file = arr_get('file', $item, '[internal function]');
+            $file = str_replace(base_path() . DIRECTORY_SEPARATOR, '', $file);
+            $line = arr_get('line', $item);
+
+            $class = arr_get('class', $item);
+            $func = arr_get('function', $item);
+            $type = arr_get('type', $item);
+            $line = $line ? '(' . $line . ')' : '';
+            $args = arr_get('args', $item, []);
+
+            $ar = '';
+            foreach ($args as $arg) {
+                $a = $arg;
+
+                if (is_object($a)) $a = get_class($a);
+
+                if (is_array($a)) {
+                    $a = '[]';
+                } else if (is_string($a)) {
+                    $a = "'" . $a . "'";
+                } else if (is_bool($a)) {
+                    $a = $a ? 'true' : 'false';
+                }
+
+                $ar .= $a . ', ';
+            }
+
+
+            $ar = rtrim($ar, ', ');
+
+            $trace[] =  '[#' . $i .'] '. $file . $line . ': ' . $class . $type . $func . '(' . $ar . ')';
+        }
+
+        return $trace;
     }
 }
